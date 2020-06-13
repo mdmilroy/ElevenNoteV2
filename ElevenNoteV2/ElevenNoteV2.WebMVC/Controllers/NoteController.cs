43e1cb1145295f99e6e0cd714def1155fafc95c2
Michael Milroy <mdmilroy@gmail.com>
2020-06-13 10:23:58 -0400

    added NoteListItem and Index View for Note controller

---
+++ ElevenNoteV2/ElevenNoteV2.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteV2.WebMVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        [Authorize]
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
